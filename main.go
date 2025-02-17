d5ec31fac95120fcea83dd0c092e61031c748427
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2025-02-17 12:41:15 +0000

    First iteration

--- main.go
+++ main.go
@@ -1,48 +1,23 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/joaooliveirapro/xmlsyncgo/src/models"
+	"github.com/joaooliveirapro/xmlsyncgo/initializers"
+	"github.com/joaooliveirapro/xmlsyncgo/models"
+	"github.com/joaooliveirapro/xmlsyncgo/parser"
 )
 
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnecToDB()
+}
+
 func main() {
-	sm, err := models.NewSyncManager(
-		"./data/config.json",
-		"./data/file_table.json",
-		"./data/job_table.json",
-	)
-	if err != nil {
-		fmt.Println(err)
-	}
+	// Get all clients from DB
+	var clients []models.Client
+	initializers.DB.Preload("Files").Find(&clients)
 
-	ok := sm.SftpStep()
-	if !ok {
-		fmt.Println("Opps...")
-		return
-	}
-	// Read remote file (using locally stored to avoid mul connections to sftp while dev)
-	err = sm.ParseXML()
-	if err != nil {
-		fmt.Println(err)
-	}
+	// Create parser manager
+	pm := parser.ParserManager{}
+	pm.Run(&clients)
 
-	// Check integrity of new remote file
-	ok = sm.FileIntegrityCheck()
-	if !ok {
-		fmt.Println("Oops...")
-	}
-
-	// Process new jobs
-	sm.ProcessJobs()
-
-	// Check for removed jobs
-	sm.CheckRemovedJobs()
-
-	sm.SaveData()
-	// Print stats
-	// sm.PrintStats()
-
-	// Clean up temp files
-	// sm.CleanUpTempFiles()
 }
