11aee64f3e4f13d847803f2f6043c99dffed3c1f
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2025-02-22 11:12:40 +0000

    Db production ready

--- initializers/database.go
+++ initializers/database.go
@@ -3,6 +3,7 @@ package initializers
 import (
 	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -11,11 +12,33 @@ import (
 
 var DB *gorm.DB
 
+func NewProductionFileLogger(filePath string) (logger.Interface, error) {
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return logger.New(
+		log.New(file, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,  // Slow SQL threshold
+			LogLevel:                  logger.Error, // Only log errors
+			IgnoreRecordNotFoundError: true,         // Ignore ErrRecordNotFound error
+			Colorful:                  false,        // Disable color
+		},
+	), nil
+}
+
 func ConnecToDB() {
 	var err error
-	DB, err = gorm.Open(sqlite.Open(os.Getenv("DB_URL")), &gorm.Config{})
+	myLogger, err := NewProductionFileLogger("./temp/db.log")
+	if err != nil {
+		log.Fatal("Error opening db log")
+	}
+	DB, err = gorm.Open(sqlite.Open(os.Getenv("DB_URL")), &gorm.Config{
+		Logger: myLogger,
+	})
 	if err != nil {
 		log.Fatal("Error connecting to DB")
 	}
-	DB.Logger.LogMode(logger.Silent)
+	DB.Logger.LogMode(logger.Error)
 }
